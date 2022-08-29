6ed86c68255a6f352d6779c3f2b981558bfedd77
Sean Trott <seantrott@Seans-MacBook-Pro.local>
2022-08-29 09:43:25 -0700

    Adding first draft

---
+++ _build/jupyter_execute/lectures/02-tooling.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Tooling and Environment
+
+# ## Goals of this lecture
+# 
+# In this lecture, we'll discuss some of the key **software tools** we'll be using throughout the course.
+# 
+# - [Jupyter notebooks](https://jupyter.org/).  
+# - [Datahub](https://datahub.io/).  
+# - [Anaconda](https://www.anaconda.com/products/distribution).  
+
+# ## What is tooling, and why is it hard?  
+# 
+# Often, one of the hardest things about learning programming is **tooling**. 
+# 
+# - There are tons of new tools you've never used before, and it can seem overwhelming.  
+# - So if you *do* feel overwhelmed, you're not alone!  
+# 
+# This means:
+# 
+# - Don't hesitate to ask us for help.  
+# 
+# 
+
+# In[ ]:
+
+
+
+
